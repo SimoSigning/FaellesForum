648946e20b227bcb03d619c7b64b1c4dac3ddd79
Simon Hylleberg Christensen <simo675z@edu.sde.dk>
2019-04-04 15:27:51 +0200

    derpmit

--- FaellesForum/Forum.aspx.cs
+++ FaellesForum/Forum.aspx.cs
@@ -6,6 +6,8 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Collections;
 using System.IO;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace FaellesForum
 {
@@ -18,20 +20,44 @@ namespace FaellesForum
 
         protected void Save_Click(object sender, EventArgs e)
         {
-            StreamWriter writer = new StreamWriter("C:\\Messages.txt", true);
-            writer.WriteLine(BeskedBox.Text);
-            writer.Close();
-            BeskedBox.Text = "";
+            /*   StreamWriter writer = new StreamWriter("C:\\Messages.txt", true);
+               writer.WriteLine(BeskedBox.Text);
+               writer.Close();
+               BeskedBox.Text = "";*/
+            SqlConnection newconnection = new SqlConnection(ConfigurationManager.ConnectionStrings["derpection"].ConnectionString);
+            string Besked = BeskedBox.Text;
+            string Brugernavn = NavnBox.Text;
+            string sql01 = "SELECT Id from Users where UserName = " + "'" + Brugernavn +"'";
+            string sql02 = "INSERT INTO Messages (Besked, BrugerID) VALUES('" + Besked + "')";
+            SqlCommand hmm01 = new SqlCommand(sql01, newconnection);
+            SqlCommand hmm02 = new SqlCommand(sql02, newconnection);
+            SqlParameter user = new SqlParameter();
+            user.ParameterName = "@username";
+            hmm01.Parameters.Add(user);
+
+            newconnection.Open();
+            hmm01.ExecuteNonQuery();
+            newconnection.Close();
+
+            
+
         }
 
         protected void Reset_Click(object sender, EventArgs e)
         {
-
+            NavnBox.Text = "";
+            EmailBox.Text = "";
+            BeskedBox.Text = "";
+            AdresseBox.Text = "";
+            MobilBox.Text = "";
         }
 
         protected void Delete_Click(object sender, EventArgs e)
         {
-
+            StreamWriter writer = new StreamWriter("C:\\Messages.txt");
+            writer.Flush();
+            writer.Close();
+            BeskedBox.Text = "";
         }
 
         protected void Read_Click(object sender, EventArgs e)
--- FaellesForum/login.aspx.cs
+++ FaellesForum/login.aspx.cs
@@ -11,7 +11,46 @@ namespace FaellesForum
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            /*            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Connection"].ConnectionString);
+            string userName = txtBrugernavn.Text;
+            string password = txtPassword.Text;
 
+            try
+            {
+                string sql = "select count(*) from UserLogin where UserName = @username and Password = @password";
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                SqlParameter user = new SqlParameter();
+                user.ParameterName = "@username";
+                user.Value = userName.Trim();
+                cmd.Parameters.Add(user);
+
+                SqlParameter pass = new SqlParameter();
+                pass.ParameterName = "@password";
+                pass.Value = password.Trim();
+                cmd.Parameters.Add(pass);
+
+                connection.Open();
+                
+                int count = (int)cmd.ExecuteScalar();
+                
+                if(count > 0)
+                {
+                    lblMessage.Text = "Login ok";
+                }
+                else
+                {
+                    lblMessage.Text = "Forkert brugernavn eller password.";
+                }
+            }
+            catch(Exception ex)
+            {
+                //Fejllog
+            }
+            finally
+            {
+                //hvis forbindelsen ikke er lukket når vi når finally, så lukkes den.
+                if (connection != null) connection.Close();
+            }*/
         }
 
         protected void CreateUserBtn_Click(object sender, EventArgs e)
